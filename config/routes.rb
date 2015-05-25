4e1da6132f5a409b6fa78d4ba45739d943aae6cd
naytlender <naytlender@gmail.com>
2015-05-25 17:59:26 +0300

    css + some static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+	
+  root to: 'static_pages#home'
+
+  get 'static_pages/about' => 'static_pages#about'
+  get 'static_pages/calc' => 'static_pages#calc'
+  get 'static_pages/contact' => 'static_pages#contact'
+
   devise_for :users
-  get 'persons/profile', as: 'user_root'
 
 end
